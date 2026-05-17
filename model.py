b92f40573d345dd28fc245fc9ae565b1c7bb3ec1
theswayamsarthak <154084784+theswayamsarthak@users.noreply.github.com>
2026-05-17 23:02:00 +0530

    Update default vocabulary sizes in model initialization

--- model.py
+++ model.py
@@ -471,13 +471,13 @@ class Transformer(nn.Module):
 
     def __init__(
         self,
-        src_vocab_size: int,
-        tgt_vocab_size: int,
-        d_model:   int   = 512,
-        N:         int   = 6,
-        num_heads: int   = 8,
-        d_ff:      int   = 2048,
-        dropout:   float = 0.1,
+        src_vocab_size: int   = 8000,
+        tgt_vocab_size: int   = 8000,
+        d_model:        int   = 512,
+        N:              int   = 6,
+        num_heads:      int   = 8,
+        d_ff:           int   = 2048,
+        dropout:        float = 0.1,
     ) -> None:
         super().__init__()
         self.d_model = d_model
